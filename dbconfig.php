78e2911d09092db3f654e227f7bdc19e5cc467bb
stavan <stavan.p.shah@gmail.com>
2017-11-14 19:45:06 +0530

    mongo string added

--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,7 @@
 <?php
 
    // connect to mongodb
-   $conn = new MongoClient();
+   $conn = new MongoClient("mongodb://rootdb:jWl09xIu2RDbC9iNCvu38T4exuVPJwV4bfVhvgdS8TNx645f7ol1bwNhs91WM6xC6UDhuCOjocMlZNlRFfQ83Q==@rootdb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb");
 	
    // select a database
    $db = $conn->moodsync;
